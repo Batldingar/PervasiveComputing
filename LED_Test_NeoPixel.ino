139e289db019d4720c25b9480f4229c6c195c091
albi <albiberberi456@yahoo.com>
2026-04-24 12:40:13 +0200

    3 LED color shift

--- LED_Test_NeoPixel.ino
+++ LED_Test_NeoPixel.ino
@@ -1,29 +1,35 @@
 #include <Adafruit_NeoPixel.h>
 
-// Choose your pins
-#define POWER_PIN    13   // This pin will act as your "VCC"
-#define DATA_PIN     12  // This pin sends the color data
-#define NUMPIXELS    2   
+#define POWER_PIN    13   
+#define DATA_PIN     12  
+#define NUMPIXELS    3   
 
 Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
+// Renamed from 'index' to 'myIndex' to avoid naming conflicts
+int myIndex = 0;
+
 void setup() {
-  // Set the Power Pin to OUTPUT and turn it HIGH (3.3V)
   pinMode(POWER_PIN, OUTPUT);
   digitalWrite(POWER_PIN, HIGH);
-
-  // Give the LED a tiny moment to wake up after receiving power
   delay(10); 
-
   pixels.begin();
 }
 
 void loop() {
-  // Set to Red
-  pixels.setPixelColor(0, pixels.Color(150, 0, 0));
-  // Set to Blue
-  pixels.setPixelColor(1, pixels.Color(0, 0, 150));
+  pixels.clear();
+
+  // Use the new variable name
+  pixels.setPixelColor(myIndex % 3,         pixels.Color(150, 0, 0)); // Red
+  pixels.setPixelColor((myIndex + 1) % 3,   pixels.Color(0, 150, 0)); // Green
+  pixels.setPixelColor((myIndex + 2) % 3,   pixels.Color(0, 0, 150)); // Blue
+
   pixels.show();
   
-  delay(1000); 
+  myIndex++; // Increment the uniquely named variable
+  
+  // Keep myIndex from growing into a massive number eventually
+  if (myIndex >= 3) myIndex = 0; 
+
+  delay(200); 
 }
