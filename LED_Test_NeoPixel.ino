2036d37a59561b94b5e917293f681c098704ed00
albi <albiberberi456@yahoo.com>
2026-04-28 12:05:34 +0200

    Update configuration and pattern logic

--- LED_Test_NeoPixel.ino
+++ LED_Test_NeoPixel.ino
@@ -2,12 +2,11 @@
 
 #define POWER_PIN    13   
 #define DATA_PIN     12  
-#define NUMPIXELS    3   
+#define NUMPIXELS    16   
 
 Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
-// Renamed from 'index' to 'myIndex' to avoid naming conflicts
-int myIndex = 0;
+int offset = 0; // shifts the pattern each time
 
 void setup() {
   pinMode(POWER_PIN, OUTPUT);
@@ -19,17 +18,23 @@ void setup() {
 void loop() {
   pixels.clear();
 
-  // Use the new variable name
-  pixels.setPixelColor(myIndex % 3,         pixels.Color(150, 0, 0)); // Red
-  pixels.setPixelColor((myIndex + 1) % 3,   pixels.Color(0, 150, 0)); // Green
-  pixels.setPixelColor((myIndex + 2) % 3,   pixels.Color(0, 0, 150)); // Blue
+  // Loop through all 16 LEDs
+  for(int i = 0; i < NUMPIXELS; i++) {
+    // This creates a repeating pattern: Red, Green, Blue, Off, Red, Green, Blue...
+    if ((i + offset) % 4 == 0) {
+      pixels.setPixelColor(i, pixels.Color(150, 0, 0));   // Red
+    } else if ((i + offset) % 4 == 1) {
+      pixels.setPixelColor(i, pixels.Color(0, 150, 0));   // Green
+    } else if ((i + offset) % 4 == 2) {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 150));   // Blue
+    }
+    // The 4th state ( % 4 == 3 ) is left as "clear"
+  }
 
   pixels.show();
   
-  myIndex++; // Increment the uniquely named variable
-  
-  // Keep myIndex from growing into a massive number eventually
-  if (myIndex >= 3) myIndex = 0; 
+  offset++; // Shift the starting point for the next frame
+  if (offset >= 4) offset = 0; 
 
-  delay(200); 
+  delay(150); // 
 }
